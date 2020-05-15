bcf7c18e13671840a1af124bf26f16e11e73c5cd
pedjo95 <tec.ped95@gmail.com>
2020-05-15 19:37:21 -0300

    Addin the repos to the UI

--- github.js
+++ github.js
@@ -1,17 +1,23 @@
 class GitHub {
   constructor() {
     this.client_ID = "14e0e468b8430006f247",
-    this.client_Secret = "ca06c1bf1edd2567ebda9eb568a07790faa36030"
+    this.client_Secret = "ca06c1bf1edd2567ebda9eb568a07790faa36030",
+    this.repos_count = 5,
+    this.repos_sort = 'created asc'
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_ID}&client_secret=${this.client_Secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_ID}&client_secret=${this.client_Secret}`);
+
     const profile = await profileResponse.json();
+
+    const repos = await repoResponse.json();
 
     
     return {
-      profile
+      profile, repos
     }
   } 
 }
--- ui.js
+++ ui.js
@@ -31,6 +31,29 @@ class UI {
     `;
   }
 
+  showRepos(repos) {
+    let output = '';
+
+    repos.forEach((repo) => {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" targert="_blank">${repo.name}
+            </div>
+            <div class="col-md-6">
+            <span class="badge badge-primary mb-2">Public Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-secondary mb-2">Watchers: ${repo.watchers_count}</span>
+            <span class="badge badge-succes mb-2">Forks: ${repo.forks_count}</span>
+            </div>
+          </div>
+        </div>
+      `; 
+    });
+
+    document.getElementById('repos').innerHTML = output;
+  }
+
   clearProfile() {
     this.profile.innerHTML = ''
   }
